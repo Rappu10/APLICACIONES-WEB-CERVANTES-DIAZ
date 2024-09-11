734efbb207a846c466bb6e73e3cea3b0f13dedf6
Francisco <poderosorappu@gmail.com>
2024-09-11 08:42:56 -0600

    Practica 2.1

---
+++ Practica 1/slider.js
@@ -0,0 +1,16 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+    if (n >= slides.length) { slideIndex = 0 }
+    if (n < 0) { slideIndex = slides.length - 1 }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
